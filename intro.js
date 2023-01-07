a496b8459f6ce60907febed14966184c3ec4e538
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-07 12:30:26 -0500

    Working on functions!

--- intro.js
+++ intro.js
@@ -1,3 +1,25 @@
-const sample = document.getElementById(input);
+function add7(a) {
+  return a + 7;
+}
 
-addEventListener(onclick, sample, alert("clicked"));
+console.log(add7(19));
+
+function muliply(a, b) {
+  return a * b;
+}
+
+console.log(muliply(8, 8));
+
+function captial(string) {
+  const lower = string.toLowerCase();
+  return lower.charAt(0).toUpperCase() + lower.slice(1);
+}
+
+console.log(captial("gOAT"));
+
+function last(string) {
+  n = string.length - 1;
+  return string.charAt(n);
+}
+
+console.log(last("imlearning!"));
