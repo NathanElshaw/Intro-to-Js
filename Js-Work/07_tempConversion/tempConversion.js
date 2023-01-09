1dd7888d15161e7d7d40e4610efb939f3bf5f5ac
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-09 18:34:27 -0500

    Lessons 7

--- Js-Work/07_tempConversion/tempConversion.js
+++ Js-Work/07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (deg) {
+  let result = ((deg - 32) * 5) / 8;
+  result = Math.round(result * 100) / 100;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (deg) {
+  let result = (deg * 9) / 5 + 32;
+  result = Math.round(result * 100) / 100;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
