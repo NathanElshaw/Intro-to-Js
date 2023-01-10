159d287f80f7c7d5fb9716f0e03a35506033eedd
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-10 18:08:54 -0500

    Project work

--- Etch-a-Sketch/core.js
+++ Etch-a-Sketch/core.js
@@ -1,4 +1,30 @@
 const app = document.getElementById("app");
+const rows = document.getElementById("inputRows");
+const cols = document.getElementById("inputCols");
+const init = document.getElementById("change");
+const resize = document.getElementById("resize");
+
+let div = document.createElement("div");
+
+init.addEventListener("click", () => {
+  makeGrid(rows.value, cols.value);
+  init.className = "hidden";
+  resize.className = "seen";
+});
+
+resize.addEventListener("click", () => {
+  console.log(rows.value);
+  console.log(cols.value);
+  const lastvalue = app.childElementCount;
+  clearGrid(lastvalue);
+});
+
+function clearGrid(num) {
+  for (i = 0; i < num; i++) {
+    app.removeChild(app.firstChild);
+  }
+  makeGrid(rows.value, cols.value);
+}
 
 function makeGrid(rows, cols) {
   app.style.setProperty("--grid-rows", rows);
@@ -9,5 +35,3 @@ function makeGrid(rows, cols) {
     app.appendChild(div).className = "grid-item";
   }
 }
-
-makeGrid(16, 16);
