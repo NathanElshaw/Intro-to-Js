d27349eeaa133026fd649d1c664a528332913358
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-11 18:29:03 -0500

    finished Styling and added boilerplate Js to calculator

---
+++ Calc/core.js
@@ -0,0 +1,26 @@
+const pastDisplay = document.querySelector("#pastDisplay");
+const currentDisplay = document.querySelector("#currentDisplay");
+
+const clear = document.querySelector("#clear");
+const enter = document.querySelector("#operate");
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+
+const multiply = document.querySelector("#muti");
+const subtract = document.querySelector("#minus");
+const add = document.querySelector("#add");
+const divide = document.querySelector("#divide");
+const decimal = document.querySelector("#decmial");
+
+zero.addEventListener("click", () => {
+  console.log("clicked");
+});
