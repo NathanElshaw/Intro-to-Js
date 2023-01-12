76c045f8a8279723ac5adaa84b7af21f3e6401f2
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-11 20:14:52 -0500

    Added some calc function.

--- Calc/core.js
+++ Calc/core.js
@@ -4,23 +4,65 @@ const currentDisplay = document.querySelector("#currentDisplay");
 const clear = document.querySelector("#clear");
 const enter = document.querySelector("#operate");
 
-const zero = document.querySelector("#zero");
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
+const calcButton = document.querySelectorAll(".btnMain");
 
-const multiply = document.querySelector("#muti");
-const subtract = document.querySelector("#minus");
-const add = document.querySelector("#add");
-const divide = document.querySelector("#divide");
-const decimal = document.querySelector("#decmial");
+let display = 0;
+let appendInt = 0;
+let currentOp = "";
 
-zero.addEventListener("click", () => {
-  console.log("clicked");
+calcButton.forEach((btnMain) => {
+  btnMain.addEventListener("click", () => {
+    updateDisplay(
+      btnMain.value,
+      currentDisplay.textContent,
+      pastDisplay.textContent
+    );
+  });
 });
+
+clear.addEventListener("click", () => {
+  clearDisplay();
+});
+
+enter.addEventListener("click", () => {
+  operate(pastDisplay.textContent, currentDisplay.textContent);
+});
+
+function clearDisplay() {
+  currentDisplay.textContent = 0;
+  pastDisplay.textContent = 0;
+  pastDisplay.className = "hidden";
+}
+
+function updateDisplay(newInt, lastInt, miniDisplay) {
+  if (newInt === "/" || newInt === "*" || newInt === "-" || newInt === "+") {
+    let cache = (pastDisplay.textContent = lastInt.concat(` `, newInt));
+    if (miniDisplay.length > 1) {
+      currentOp = currentOp + cache;
+      display = miniDisplay;
+      pastDisplay.textContent = cache;
+      console.log(currentOp);
+    }
+    currentOp = cache;
+    pastDisplay.className = "shown";
+    currentDisplay.textContent = 0;
+  } else {
+    if (lastInt > 0) {
+      appendInt = newInt;
+      currentDisplay.textContent = lastInt.concat(newInt);
+    } else {
+      currentDisplay.textContent = newInt;
+    }
+  }
+}
+
+function operate(past, current) {
+  console.log(past, current);
+}
+
+/* 
+Get button input -Done
+keep a running string of numbers before a logic op is used - Done
+add second, and so on input - Done
+evalute the function
+*/
